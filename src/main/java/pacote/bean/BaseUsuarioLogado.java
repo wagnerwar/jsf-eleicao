412800a871f239dfd1ebd73ec5f91dc9ded428b1
Wagner <nuneswar2000@hotmail.com>
2018-03-28 17:57:38 -0300

    Implementação de autenticação de usuário logado

--- src/main/java/pacote/bean/BaseUsuarioLogado.java
+++ src/main/java/pacote/bean/BaseUsuarioLogado.java
@@ -2,13 +2,17 @@ package pacote.bean;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class BaseUsuarioLogado extends Base {
+	
 	@PostConstruct
 	public void init() {
+		System.out.print("Chamada função de checagem de login");
 		if(this.autenticaUsuario() == false) {
 			FacesContext facesContext = FacesContext.getCurrentInstance();
 			HttpServletResponse resposta = (HttpServletResponse) facesContext.getExternalContext().getResponse();
--- src/main/java/pacote/bean/Index.java
+++ src/main/java/pacote/bean/Index.java
@@ -2,19 +2,29 @@ package pacote.bean;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.PreRenderViewEvent;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 @ManagedBean(name = "index")
+@SessionScoped()
 public class Index extends BaseUsuarioLogado {
 
 	public String mensagem;
 	
 	@PostConstruct
 	public void init() {
-		super.init();
+		System.out.print("Chamada função INIT");
+	}
+	
+	public void before(ComponentSystemEvent event) {
+		System.out.print("Chamada função BEFORE");
 	}
 	
 }
