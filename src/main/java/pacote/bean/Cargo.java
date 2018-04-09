6e9dddcdb3432e1fab1669549454406ac06b57ca
Wagner <nuneswar2000@hotmail.com>
2018-04-09 15:56:34 -0300

    Configuração de banco de dados

---
+++ src/main/java/pacote/bean/Cargo.java
@@ -0,0 +1,62 @@
+package pacote.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.PreRenderViewEvent;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.bson.Document;
+import pacote.dao.ConexaoMongo;
+import com.mongodb.client.MongoDatabase; 
+import com.mongodb.MongoClient; 
+import com.mongodb.client.MongoCollection; 
+
+@ManagedBean(name = "cargo")
+@ViewScoped
+public class Cargo {
+	private String nome;
+	private String descricao;
+	private String status;
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void cadastrar() {
+		try {
+			ConexaoMongo conn = new ConexaoMongo();
+			MongoDatabase db = conn.getDb();
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "error", ex.getMessage()));
+			
+		}
+	}
+}
---
+++ src/main/java/pacote/dao/ConexaoMongo.java
@@ -0,0 +1,36 @@
+package pacote.dao;
+
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.MongoClient; 
+import com.mongodb.client.MongoCollection; 
+
+public class ConexaoMongo {
+	public static String db = "eleicao";
+	
+	public MongoClient getConexao() {
+		MongoClient mongo = new MongoClient( "localhost" , 27017 );
+		return mongo;
+	}
+	
+	public MongoDatabase getDb() {
+		MongoClient conn = this.getConexao();
+		return conn.getDatabase(ConexaoMongo.db);
+	}
+	
+	public MongoCollection<Document> getColecao(String colecao) {
+		
+		MongoCollection<Document> lista = this.getDb().getCollection(colecao);
+		if (lista == null){
+			try {
+				this.getDb().createCollection(colecao);
+			}catch(Exception ex) {
+				ex.printStackTrace();
+				return null;
+			}
+			return this.getDb().getCollection(colecao);
+		}else {
+			return lista;
+		}
+	}	
+}
