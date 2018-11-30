7387297084292a7997f1c4ef35060fe5a98c0aa2
Wagner <nuneswar2000@hotmail.com>
2018-11-30 18:31:40 -0200

    Ajustes diversos

--- src/main/java/pacote/servlet/Candidato.java
+++ src/main/java/pacote/servlet/Candidato.java
@@ -3,8 +3,16 @@ package pacote.servlet;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
+
+import pacote.bean.CandidatoBean;
+import pacote.dao.EleicaoDB;
 
 @ManagedBean(name = "candidato")
 @ViewScoped
@@ -14,51 +22,35 @@ public class Candidato extends BaseUsuarioLogado implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int id;
-	private String nome;
-	private String cpf;
-	private Date data_nascimento;
-	private String genero;
+	private CandidatoBean informacoes;
 	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public int getId() {
-		return this.id;
+	public CandidatoBean getInformacoes() {
+		return informacoes;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Date getDataNascimento() {
-		return data_nascimento;
-	}
-
-	public void setDataNascimento(Date data_nascimento) {
-		this.data_nascimento = data_nascimento;
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public void setGenero(String genero) {
-		this.genero = genero;
+	public void setInformacoes(CandidatoBean informacoes) {
+		this.informacoes = informacoes;
 	}
 	
+	public void salvar() {
+		
+	}
+	
+	@PostConstruct
+    public void init() {
+		try {
+			this.inicializar();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "error", ex.getMessage()));			
+		}
+    }
+	
+	public void inicializar() {
+		System.out.println("CandidatoBean inicializado");
+		this.informacoes = new CandidatoBean();
+		this.informacoes.setCpf(null);
+		RequestContext.getCurrentInstance().reset("frmCandidato");
+	}
 	
 }
